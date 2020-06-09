e3c076a8443d8be90adde5a21095ec98091f2c49
Shmelev Yury <yura0512@gmail.com>
2020-06-09 18:26:47 +0300

    add changes

--- class.py
+++ class.py
@@ -5,9 +5,9 @@ import numba
 
 
 class Panorama:
-    def __init__(self, file_name, step=10):
+    def __init__(self, file_name, shift=10):
         self.file_name = file_name
-        self.step = step
+        self.shift = shift
 
     @staticmethod
     def _FindFeatures(frame):
@@ -48,7 +48,7 @@ class Panorama:
         s = 4
         iter_num = 0
         indexes = np.arange(num_of_points)
-        inlier_ratio = 0.7
+        inlier_ratio = 0.73
         num_of_iterations = np.log(1 - p) / np.log(1 - (1 - inlier_ratio) ** s)
 
         all_scores = []
@@ -67,27 +67,31 @@ class Panorama:
         all_scores = np.asarray(all_scores)
         all_points = np.asarray(all_points)
 
-        best_points_id = all_scores.argsort()[::-1]
+        ind = all_scores.argmax()
+        cur_inliers = all_scores[ind]
+        prev_inliers = None
+        H_4 = panorama.getH(ptsL[all_points[ind]], ptsR[all_points[ind]])
 
-        for ind in best_points_id:
-            H_4 = panorama.getH(ptsL[all_points[ind]], ptsR[all_points[ind]])
+        while True:
             transformed_4 = cv2.perspectiveTransform(np.array([ptsL]), H_4)
             inliers_mask = (Panorama.norm(transformed_4[0], ptsR) < thresh)
 
             H_all = panorama.getH(ptsL[inliers_mask], ptsR[inliers_mask])
+
             transformed_all = cv2.perspectiveTransform(np.array([ptsL[inliers_mask]]), H_all)
+            prev_inliers = cur_inliers
+            cur_inliers = (Panorama.norm(transformed_all[0], ptsR[inliers_mask]) < thresh).sum()
 
-            if not use_mod or (Panorama.norm(transformed_all[0], ptsR[inliers_mask]) < thresh).sum() >= all_scores[ind]:
+            if cur_inliers <= prev_inliers:
+                return H_4
+            elif not use_mod:
                 return H_all
-
-        return None
-
-
+            H_4 = H_all
 
     @staticmethod
     def _FindTransform(keypointsL, keypointsR, featuresL, featuresR, use_custom=False, use_mod=True):
-        ratio = 0.75
-        thresh = 2
+        ratio = 0.85
+        thresh = 5
         matcher = cv2.DescriptorMatcher_create("BruteForce")
         rawMatches = matcher.knnMatch(featuresL, featuresR, 2)
         matches = []
@@ -108,9 +112,9 @@ class Panorama:
 
     def make(self, show_process=False, show_result=True, use_custom=False, use_mod=True):
         cap = cv2.VideoCapture(self.file_name)
+        last_x = 0
 
         ret, total_image = cap.read()
-        skipped_frames = 0
 
         for _ in tqdm(range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT) - 1))):
             ret, frame = cap.read()
@@ -121,43 +125,52 @@ class Panorama:
             keypointsL, featuresL = self._FindFeatures(total_image)
             keypointsR, featuresR = self._FindFeatures(frame)
 
-            if skipped_frames > self.step:
 
-                ptsL, ptsR, H, status = self._FindTransform(keypointsR, keypointsL, featuresR, featuresL,
-                                                            use_custom=use_custom, use_mod=use_mod)
+            ptsL, ptsR, H, status = self._FindTransform(keypointsR, keypointsL, featuresR, featuresL,
+                                                        use_custom=use_custom, use_mod=use_mod)
 
-                if H is not None:
-                    if show_process:
-                        cpy = frame.copy()
+            if H is not None:
+                if show_process:
+                    cpy = frame.copy()
 
-                    up_and_down = np.array([[[frame.shape[1], 0], [frame.shape[1], frame.shape[0]]]], dtype=np.float)
-                    after_points = cv2.perspectiveTransform(up_and_down, H)
-                    new_x = int(min(after_points[0][0][0], after_points[0][1][0]))
+                up_and_down = np.array([[[frame.shape[1], 0], [frame.shape[1], frame.shape[0]]]], dtype=np.float)
+                after_points = cv2.perspectiveTransform(up_and_down, H)
+                new_x = int(min(after_points[0][0][0], after_points[0][1][0]))
 
-                    frame = cv2.warpPerspective(frame, H, (frame.shape[1] + total_image.shape[1], frame.shape[0]))
+                if new_x - last_x < self.shift:
+                    continue
 
-                    frame[0: total_image.shape[0], 0: total_image.shape[1]] = total_image
-                    total_image = frame[:, 0: new_x]
+                last_x = new_x
 
-                    while show_process:
-                        # colours = sns.color_palette(n_colors=status.size)
+                frame = cv2.warpPerspective(frame, H, (frame.shape[1] + total_image.shape[1], frame.shape[0]))
+                mask = np.ones(frame.shape, dtype=frame.dtype) * 255
+                mask = cv2.warpPerspective(mask, H, (frame.shape[1] + total_image.shape[1], frame.shape[0]))
+                mask = (255 - mask[0: total_image.shape[0], 0: total_image.shape[1]]).astype(np.float) / 255
 
-                        # colour_count = 0
-                        # for i in range(status.size):
-                        #     if status[i]:
-                        #         cv2.circle(cpy, (int(ptsL[i][0]), int(ptsL[i][1])), 2, colours[i], thickness=-1)
-                        #         cv2.circle(frame, (int(ptsR[i][0]), int(ptsR[i][1])), 2, colours[i], thickness=-1)
-                        #         colour_count += 1
+                interlap_cpy = frame[0: total_image.shape[0], 0: total_image.shape[1]].astype(np.float)
+                interlap_cpy += (total_image.astype(np.float) + mask * total_image.astype(np.float))
+                interlap_cpy /= 2
+                frame[0: total_image.shape[0], 0: total_image.shape[1]] = interlap_cpy.astype(np.uint8)
+                total_image = frame[:, 0: new_x]
 
-                        cv2.imshow('right', cpy)
-                        # cv2.imshow('right', frame)
-                        # cv2.imshow('after transform', cv2.warpPerspective(cpy, H, (total_width, total_height)))
-                        cv2.imshow('total', total_image)
-                        if cv2.waitKey(30) & 0xFF == ord('q'):
-                            break
-                    skipped_frames = 0
+                while show_process:
+                    # colours = sns.color_palette(n_colors=status.size)
 
-            skipped_frames += 1
+                    # colour_count = 0
+                    # for i in range(status.size):
+                    #     if status[i]:
+                    #         cv2.circle(cpy, (int(ptsL[i][0]), int(ptsL[i][1])), 2, colours[i], thickness=-1)
+                    #         cv2.circle(frame, (int(ptsR[i][0]), int(ptsR[i][1])), 2, colours[i], thickness=-1)
+                    #         colour_count += 1
+
+                    # real_mask = np.ones(frame.shape, dtype=frame.dtype) * 255
+                    # real_mask = cv2.warpPerspective(real_mask, H, (frame.shape[1] + total_image.shape[1], frame.shape[0]))
+                    # real_mask = real_mask[0: total_image.shape[0], 0: total_image.shape[1]]
+
+                    # cv2.imshow('right', cpy)
+                    cv2.imshow('total', total_image)
+                    if cv2.waitKey(30) & 0xFF == ord('q'):
+                        break
 
         cv2.imwrite('result.png', total_image)
 
@@ -171,5 +184,5 @@ class Panorama:
 
 #%%
 if __name__ == '__main__':
-    panorama = Panorama("vid.mp4", step=10)
+    panorama = Panorama("vid.mp4", shift=100)
     panorama.make(show_process=False, show_result=True, use_custom=True, use_mod=True)
